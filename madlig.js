0f2dfb318579c8924dda563ca8ed2d30239acacf
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-04-02 13:10:51 -0700

    completed activity

--- madlig.js
+++ madlig.js
@@ -17,18 +17,52 @@ var loop = 0;
 Madlib();
 
 var getWords = function (loop) {
-    concole.log(loop);
+    console.log(nouns);
+    console.log(adjective);
+    console.log(verb);
     if (loop < 2) {
-        inquirer.prompt([
-            {
-                name: "noun1",
-                message: "Enter a noun: "
-            }
-        ]).then(function (answers) {
+        inquirer.prompt({
+            name: "noun",
+            message: "Enter a noun: "
+        }).then(function (answers) {
+            nouns.push(answers.noun);
             loop++;
             getWords(loop);
         })
     }
+    if (loop >= 2 && loop < 3) {
+        inquirer.prompt({
+            name: "adjective",
+            message: "Enter an adjective: "
+        }).then(function (answers) {
+            adjective.push(answers.adjective);
+            loop++;
+            getWords(loop);
+        })
+    }
+    if (loop >= 3 && loop < 4) {
+        inquirer.prompt({
+            name: "verb",
+            message: "Enter a verb: "
+        }).then(function (answers) {
+            verb.push(answers.verb);
+            var madLib = new Madlib(nouns, adjective, verb);
+            console.log(madLib);
+        })
+    }
+}
+
+var constructStory = function (completeObject) {
+    for (i = 0; i < 2; i++) {
+        constructStory.story = completeObject.story.replace("[noun]", completeObject.nouns[i]);
+    }
+    for (i = 0; i < 2; i++) {
+        constructStory.story = completeObject.story.replace("[adjective]", completeObject.adjective[i]);
+    }
+    for (i = 0; i < 2; i++) {
+        constructStory.story = completeObject.story.replace("[verb]", completeObject.verb[i]);
+    }
+    console.log(completeObject.story);
 }
 
 getWords(loop);
